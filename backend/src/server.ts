35c07f27752bd875469a929fe9e153c2915d7dc6
“Emiltpsgit config --global user.email “emiltps@yahoo.co.uk <emiltps@yahoo.co.uk>
2025-10-17 19:12:07 +0200

    add cors middleware to backend server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import artworksRouter from "./routes/artworks.js";
 
 export const app = express();
 
 console.log("HARVARD_API_KEY loaded:", !!process.env.HARVARD_API_KEY);
+
+app.use(
+  cors({
+    origin: "https://exhibition-curator-1.onrender.com",
+  })
+);
 
 //Route to fetch artwork
 app.use(express.json());
