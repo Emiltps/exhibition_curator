faccf5a583c560bc478754af5dcdc850dfa98d0b
“Emiltpsgit config --global user.email “emiltps@yahoo.co.uk <emiltps@yahoo.co.uk>
2025-10-17 18:37:15 +0200

    update key logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import artworksRouter from "./routes/artworks.js";
 
 export const app = express();
 
-console.log("HARVARD_API_KEY loaded:", process.env.HARVARD_API_KEY); // should log the actual key
+console.log("HARVARD_API_KEY loaded:", !!process.env.HARVARD_API_KEY);
 
 //Route to fetch artwork
 app.use(express.json());
